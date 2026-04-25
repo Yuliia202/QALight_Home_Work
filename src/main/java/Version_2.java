5828a2c4315d369819cb77e6e28ea69d242d8c99
Yulia <palchikgyy@gmail.com>
2026-04-25 22:09:35 +0300

    Version 2

---
+++ src/main/java/Version_2.java
@@ -0,0 +1,7 @@
+import java.util.Arrays;
+
+public class Version_2 {
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, 7};
+        System.out.println(Arrays.toString(array));    }
+}
